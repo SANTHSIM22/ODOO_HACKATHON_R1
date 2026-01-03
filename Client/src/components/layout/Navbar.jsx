835a6e63b4d72a01f34ad5c11d934ac6537669d2
RagedRebel <reubenav1305@gmail.com>
2026-01-03 14:20:45 +0530

    fix:navbar and footer change

--- Client/src/components/layout/Navbar.jsx
+++ Client/src/components/layout/Navbar.jsx
@@ -4,7 +4,6 @@ import { Menu, X } from "lucide-react";
 function Navbar({ isMobileMenuOpen, setIsMobileMenuOpen }) {
     const navLinks = [
         { label: "Features", href: "/#features" },
-        { label: "How It Works", href: "/#how-it-works" },
         { label: "About", href: "/#about" },
         { label: "Testimonials", href: "/#testimonials" },
     ];
